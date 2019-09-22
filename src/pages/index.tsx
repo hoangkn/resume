6bc2a11529fc3ffee28c861b64744e2360c8c989
Hoang Khac Nguyen <hoangkn_dev@icloud.com>
2019-09-22 13:40:54 +0200

    Replace Bootstrap with material-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
-import "../global.scss"
-
+import { createMuiTheme, CssBaseline } from "@material-ui/core"
+import { ThemeProvider } from "@material-ui/styles"
 import React from "react"
+import "typeface-roboto"
 
 import SEO from "../components/seo"
 
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+})
+
 const IndexPage = () => (
-  <div>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-  </div>
+  </ThemeProvider>
 )
 
 export default IndexPage
